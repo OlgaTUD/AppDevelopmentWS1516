8474e6cbad1664950c33a4289654feeb79d1116f
A.Pitkin <olpitkin@gmail.com>
2015-12-17 16:34:37 +0100

    OpenWeather

--- MyPlaces/app/src/main/java/com/rn/myplaces/myplaces/com/rn/myplaces/settings/ImprintFragment.java
+++ MyPlaces/app/src/main/java/com/rn/myplaces/myplaces/com/rn/myplaces/settings/ImprintFragment.java
@@ -1,19 +1,31 @@
 package com.rn.myplaces.myplaces.com.rn.myplaces.settings;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.rn.myplaces.myplaces.MainActivity;
 import com.rn.myplaces.myplaces.R;
+import com.rn.myplaces.myplaces.weather.JSONWeatherParser;
+import com.rn.myplaces.myplaces.weather.Weather;
+import com.rn.myplaces.myplaces.weather.WeatherHttpClient;
+
+import org.json.JSONException;
 
 /**
  * Created by katamarka on 04/11/15.
  */
 public class ImprintFragment extends Fragment {
+
+    private TextView weatherView;
+    String city = "London,UK";
+    Weather weather;
 
     public static ImprintFragment newInstance() {
         ImprintFragment fragment = new ImprintFragment();
@@ -26,6 +38,19 @@ public class ImprintFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.imprint, container, false);
+
+        //TEST WEATHER STUFF
+        final TextView weatherView = (TextView) rootView.findViewById(R.id.textView1);
+        Button button = (Button)rootView.findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                weatherView.setText(weather.currentCondition.getDescr());
+            }
+        });
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city});
+
         return rootView;
     }
 
@@ -34,6 +59,40 @@ public class ImprintFragment extends Fragment {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(4);
     }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            weather = new Weather();
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+            //weatherView.setText(weather.toString());
+           // condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+           // temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "C");
+           // hum.setText("" + weather.currentCondition.getHumidity() + "%");
+           // press.setText("" + weather.currentCondition.getPressure() + " hPa");
+           // windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+           // windDeg.setText("" + weather.wind.getDeg() + "");
+
+        }
+
 }
+}
+
+
 
 
---
+++ MyPlaces/app/src/main/java/com/rn/myplaces/myplaces/weather/WeatherHttpClient.java
@@ -0,0 +1,55 @@
+package com.rn.myplaces.myplaces.weather;
+
+/**
+ * Created by Uto4ko on 17.12.2015.
+ */
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherHttpClient {
+
+
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+    public String getWeatherData(String location) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(BASE_URL + location +"&APPID=96a179f766bd8b881e21440b0cbc0762")).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            is.close();
+            con.disconnect();
+            System.out.println(buffer.toString());
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
+    }
+
+}
