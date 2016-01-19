1d4747f63c8dc3740f65a52aaeea1724fa3e9883
A.Pitkin <olpitkin@gmail.com>
2016-01-19 15:17:23 +0100

    PlacePicker + Manifest

--- MyPlaces/app/src/main/java/com/rn/myplaces/myplaces/com/rn/myplaces/settings/ImprintFragment.java
+++ MyPlaces/app/src/main/java/com/rn/myplaces/myplaces/com/rn/myplaces/settings/ImprintFragment.java
@@ -1,29 +1,21 @@
 package com.rn.myplaces.myplaces.com.rn.myplaces.settings;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-
 import com.rn.myplaces.myplaces.MainActivity;
 import com.rn.myplaces.myplaces.R;
-import com.rn.myplaces.myplaces.weather.JSONWeatherParser;
-import com.rn.myplaces.myplaces.weather.Weather;
-import com.rn.myplaces.myplaces.weather.WeatherHttpClient;
 
-import org.json.JSONException;
+
 
 /**
  * Created by katamarka on 04/11/15.
  */
 public class ImprintFragment extends Fragment {
-
-    private TextView weatherView;
 
     public static ImprintFragment newInstance() {
         ImprintFragment fragment = new ImprintFragment();
